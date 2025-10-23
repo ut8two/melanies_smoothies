6d5d7e57fc9c42a8a99fd23ec3ae18e960743ada
ut8two <53795849+ut8two@users.noreply.github.com>
2025-10-23 10:55:49 -0600

    Update streamlit_app.py
    updated cnx statement

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@
 import streamlit as st
 from snowflake.snowpark.functions import col
 # from snowflake.snowpark.context import get_active_session
-cnx = st.connection("Snowflake")
+cnx = st.connection("snowflake")
 session = cnx.session()
 
 
