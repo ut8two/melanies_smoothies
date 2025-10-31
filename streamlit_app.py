69109658ac5b0bc12e4e2b39be9506ec8f8fcf12
ut8two <53795849+ut8two@users.noreply.github.com>
2025-10-31 13:03:33 -0600

    Update streamlit_app.py
    chanign session

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,7 +18,8 @@ st.write(
 name_on_order = st.text_input('Name on Smoothie:')
 st.write('The name on the smoothie will be', name_on_order)
 
-session = get_active_session()
+# session = get_active_session()
+session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
 
